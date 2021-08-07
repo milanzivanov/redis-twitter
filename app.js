834dbff32c32dc3de866ddace706fcbe383abbea
milan.zivanov <milan.zivanov@gmail.com>
2021-08-07 17:07:12 +0200

    Setup the Pug templating engine

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
+const path = require("path");
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-    res.send("Hello Worls");
+    res.render("index");
 });
 
 app.listen(3000, (req, res) => {
