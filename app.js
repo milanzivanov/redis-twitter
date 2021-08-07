36874c8b62599a602a1d7dee782bc0e0651b0aeb
milan.zivanov <milan.zivanov@gmail.com>
2021-08-07 18:02:54 +0200

    Receive the login and password in Express

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-const path = require("path");
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
+
+// get
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+//  post
+app.post("/", (req, res) => {
+    const { username, password } = req.body;
+  
+    if (!username || !password) {
+        res.render("error", {
+            message: "Please set both username and password",
+        })
+        return
+    }
+  
+    console.log(req.body, username, password);
+    res.end();
 });
 
 app.listen(3000, (req, res) => {
